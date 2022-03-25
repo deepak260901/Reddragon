aeb3b6b51242958864ea4ce00ebb8ef41a2eb97d
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-26 05:03:17 +0530

    Update packages.d.ts

--- src/typings/packages.d.ts
+++ src/typings/packages.d.ts
@@ -30,3 +30,5 @@ declare module "ordinal";
 declare module "parse-ms-js";
 declare module "@shineiichijo/marika";
 declare module "anime-quiz";
+declare module "images-generator";
+declare module "lolis.life";
