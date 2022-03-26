3eca53a6c0d9d36dc56fcbb37c8a4e6185c72553
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-26 07:25:47 +0530

    Update Bank.ts

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "levi",
+      footerText: "© HEY-ZERO-TWO💕🤍",
       buttons: buttons,
       headerType: 1,
     };
