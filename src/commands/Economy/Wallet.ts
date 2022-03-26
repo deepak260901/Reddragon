18a4eeb38378e0f412dc7d817e1911c479afac02
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-26 07:33:06 +0530

    Update Wallet.ts

--- src/commands/Economy/Wallet.ts
+++ src/commands/Economy/Wallet.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "levi",
+      footerText: "💕zerotwo🤍",
       buttons: buttons,
       headerType: 1,
     };
